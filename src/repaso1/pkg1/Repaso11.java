a61d0c195c0c686db7bace64218c4d0086675a4a
Julian- <Julian-@Julian>
2017-02-21 05:46:14 -0500

    Repaso 1 - Objetos y Clase Primera

---
+++ src/repaso1/pkg1/Repaso11.java
@@ -0,0 +1,12 @@
+package repaso1.pkg1;
+
+public class Repaso11 {
+
+    public int size;
+    public String bread;
+    public String name;
+    
+    public void bark(){
+         System.out.println("Ruff! Wou");
+     }
+}
